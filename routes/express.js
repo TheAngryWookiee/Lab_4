57fb47fa500ba55abe6fe8b6154869d5b419e39f
TheAngryWookiee <thethompson@live.ca>
2024-04-19 23:01:48 -0400

    minor tweaks

--- routes/express.js
+++ routes/express.js
@@ -1,4 +1,16 @@
 app.get('/', (req, res) => {
-    res.render('index', { pageTitle: 'Home' });
+    res.render('index', { pageTitle: 'index' });
+  });
+
+app.get('/animals', (req, res) => {
+    res.render('all-animals'); 
+});
+  
+app.get('/animals/new', (req, res) => {
+    res.render('entry-form', { pageTitle: 'Entry Form' });
   });
   
+ 
+app.get('/animals/:id/edit', (req, res) => {
+    res.render('edit-animal', { pageTitle: 'Edit Animal' });
+  });
